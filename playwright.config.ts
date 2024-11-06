2c38080aa3162126b2ccb7bc98281dd44b255a42
Lorenzo <lorenzotrevizoldalbo@gmail.com>
2024-11-06 20:10:04 -0300

    config update

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,8 @@
-// playwright.config.js
 module.exports = {
     testDir: './tests',
     timeout: 30000,
     retries: 0,
+    reporter: [['html', { outputFolder: 'playwright-report' }]],
     use: {
       headless: true,
       viewport: { width: 1280, height: 720 },
